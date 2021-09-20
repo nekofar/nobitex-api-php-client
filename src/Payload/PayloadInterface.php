b94550be3c01861ec05f2807ad16a044d444eec7
Milad Nekofar <milad@nekofar.com>
2021-09-20 16:41:56 +0430

    feat: add `getCode` and `getMessage` methods to `Payload` instance based on the general considerations
    https://apidocs.nobitex.ir/#d9a7503222

--- src/Payload/PayloadInterface.php
+++ src/Payload/PayloadInterface.php
@@ -11,11 +11,23 @@ declare(strict_types=1);
 namespace Nekofar\Nobitex\Payload;
 
 /**
+ * @link https://apidocs.nobitex.ir/#d9a7503222
  *
+ * @property string|null $status
+ * @property string|null $code
+ * @property string|null $message
  */
 interface PayloadInterface
 {
     /**
      */
     public function getStatus(): ?string;
+
+    /**
+     */
+    public function getCode(): ?string;
+
+    /**
+     */
+    public function getMessage(): ?string;
 }
